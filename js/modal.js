cab85a9e9022426a4f10f07f816a7470e6279a4e
alexnik197 <nikiforov197@gmail.com>
2022-04-07 21:41:45 +0700

    create: burger, ankchors for a & media for modal

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// Code for modal
 let modalWindow = $(".modal")
 let buttonModal = $(".button-modal");
 let buttonModalClose = $(".modal-form__data-close")
@@ -6,8 +7,30 @@ buttonModalClose.on("click", hideModal);
 
 function showModal () {
     modalWindow.addClass("modal--active");
+    burgerWindow.removeClass("burger--active");
 }
 
 function hideModal () {
     modalWindow.removeClass("modal--active");
+}
+
+
+
+// Code for burger
+let buttonBurgerModal = $(".button-burger");
+let buttonBurger = $(".menu-button__link");
+let burgerWindow = $(".burger")
+let buttonBurgerClose = $(".burger-close")
+let buttonBurgerList = $(".burger-list__el")
+buttonBurgerModal.on("click", showModal);
+buttonBurger.on("click", showBurger);
+buttonBurgerClose.on("click", hideBurger);
+buttonBurgerList.on("click", hideBurger);
+
+function showBurger () {
+    burgerWindow.addClass("burger--active");
+}
+
+function hideBurger () {
+    burgerWindow.removeClass("burger--active");
 }
