0616f8e85d2ecc7078e579d93b04b745e56086e3
alexnik197 <nikiforov197@gmail.com>
2022-05-15 01:41:04 +0700

    edit: rewrite jquery code to vanilla js

--- js/catalog.js
+++ js/catalog.js
@@ -1,127 +1,56 @@
-let buttonOne = $(".catalog__button--1");
-let buttonTwo = $(".catalog__button--2");
-let buttonThree = $(".catalog__button--3");
-let buttonFour = $(".catalog__button--4");
-let buttonFive = $(".catalog__button--5");
-let buttonSix = $(".catalog__button--6");
-let buttonSeven = $(".catalog__button--7");
-let buttonEight = $(".catalog__button--8");
-let buttonNine = $(".catalog__button--9");
-let linkOne = $(".splide__list--1");
-let linkTwo = $(".splide__list--2");
-let linkThree = $(".splide__list--3");
-let linkFour = $(".splide__list--4");
-let linkFive = $(".splide__list--5");
-let linkSix = $(".splide__list--6");
-let linkSeven = $(".splide__list--7");
-let linkEight = $(".splide__list--8");
-let linkNine = $(".splide__list--9");
-buttonOne.on("click", showSplideOne);
-buttonTwo.on("click", showSplideTwo);
-buttonThree.on("click", showSplideThree);
-buttonFour.on("click", showSplideFour);
-buttonFive.on("click", showSplideFive);
-buttonSix.on("click", showSplideSix);
-buttonSeven.on("click", showSplideSeven);
-buttonEight.on("click", showSplideEight);
-buttonNine.on("click", showSplideNine);
+// Set variables
+const btnName = '#btn';
+const splideName = '#splide';
+const btnTotal = 9;
 
-function showSplideOne () {
-    linkOne.removeClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
+
+// Declarate functions
+function addClsBtn (btnMain, num) {
+    document.querySelector(btnMain + num).addEventListener('click', function(){
+        document.querySelector(btnMain + num).classList.add('button-active');
+        function delClsBtn () {
+            for (let i = 1; i <= btnTotal; i++) {
+                if (i != num)
+                document.querySelector(btnMain + i).classList.remove('button-active');
+            }
+        }
+        delClsBtn();
+    });
 }
-function showSplideTwo () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.removeClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
+
+function addClsSlide (button, slide, num) {
+    document.querySelector(button + num).addEventListener('click', function() {
+        document.querySelector(slide + num).classList.remove('splide__list--hide');
+        function addClsSlide () {
+            for (let i = 1; i <= btnTotal; i++) {
+                if (i != num)
+                document.querySelector(slide + i).classList.add('splide__list--hide');
+            }
+        }
+        addClsSlide();
+    });
 }
-function showSplideThree () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.removeClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
-}
-function showSplideFour () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.removeClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
-}
-function showSplideFive () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.removeClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
-}
-function showSplideSix () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.removeClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
-}
-function showSplideSeven () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.removeClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
-}
-function showSplideEight () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.removeClass("splide__list--hide");
-    linkNine.addClass("splide__list--hide");
-}
-function showSplideNine () {
-    linkOne.addClass("splide__list--hide");
-    linkTwo.addClass("splide__list--hide");
-    linkThree.addClass("splide__list--hide");
-    linkFour.addClass("splide__list--hide");
-    linkFive.addClass("splide__list--hide");
-    linkSix.addClass("splide__list--hide");
-    linkSeven.addClass("splide__list--hide");
-    linkEight.addClass("splide__list--hide");
-    linkNine.removeClass("splide__list--hide");
+
+
+// Calling functions
+for (let btnNum = 1; btnNum <= btnTotal; btnNum++)  {
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsBtn(btnName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+    addClsSlide (btnName, splideName, btnNum);
+
 }
--- js/modal.js
+++ js/modal.js
@@ -1,36 +1,11 @@
-// Code for modal
-let modalWindow = $(".modal")
-let buttonModal = $(".button-modal");
-let buttonModalClose = $(".modal-form__data-close")
-buttonModal.on("click", showModal);
-buttonModalClose.on("click", hideModal);
-
-function showModal () {
-    modalWindow.addClass("modal--active");
-    burgerWindow.removeClass("burger--active");
-}
-
-function hideModal () {
-    modalWindow.removeClass("modal--active");
+const openModal = document.querySelectorAll('.modal-call');
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener('click', function(){
+        document.querySelector('.modal').classList.add('modal-open')
+    });
 }
 
 
-
-// Code for burger
-let buttonBurgerModal = $(".button-burger");
-let buttonBurger = $(".menu-button__link");
-let burgerWindow = $(".burger")
-let buttonBurgerClose = $(".burger-close")
-let buttonBurgerList = $(".burger-list__el")
-buttonBurgerModal.on("click", showModal);
-buttonBurger.on("click", showBurger);
-buttonBurgerClose.on("click", hideBurger);
-buttonBurgerList.on("click", hideBurger);
-
-function showBurger () {
-    burgerWindow.addClass("burger--active");
-}
-
-function hideBurger () {
-    burgerWindow.removeClass("burger--active");
-}
+document.querySelector('#modal-close').addEventListener('click', function(){
+    document.querySelector('.modal').classList.remove('modal-open')
+});
