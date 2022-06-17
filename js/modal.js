80c423039e5c4dde447454e15f909b491b1b89f6
alexnik197 <nikiforov197@gmail.com>
2022-06-17 10:08:53 +0700

    edit: del parallax; replace imgs in slider

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,10 @@
-const openModal = document.querySelectorAll('.modal-call');
+const openModal = document.querySelectorAll(".modal-call");
 for (let i = 0; i < openModal.length; i++) {
-    openModal[i].addEventListener('click', function(){
-        document.querySelector('.modal').classList.add('modal-open')
-    });
+  openModal[i].addEventListener("click", function () {
+    document.querySelector(".modal").classList.add("modal-open");
+  });
 }
 
-
-document.querySelector('#modal-close').addEventListener('click', function(){
-    document.querySelector('.modal').classList.remove('modal-open')
+document.querySelector("#modal-close").addEventListener("click", function () {
+  document.querySelector(".modal").classList.remove("modal-open");
 });
